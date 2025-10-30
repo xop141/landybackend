f4168f14aa41fb1b8c637cc30a2b00dc9b95829c
xop141 <lowsens400@gmail.com>
2025-10-31 02:38:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ preloadCafeCache();
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.FRONT_TEST],
+    origin: [process.env.FRONT_LINK],
   })
 );
 const MONGO = process.env.MONGO;
